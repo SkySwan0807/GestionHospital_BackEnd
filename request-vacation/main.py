ac1482994a9525027d5ccabe6c13badd59d2c426
Diego Solano <solanoterandiego@gmail.com>
2026-03-10 11:21:35 -0400

    Update main.py

--- request-vacation/main.py
+++ request-vacation/main.py
@@ -1,51 +1,101 @@
+from fastapi import FastAPI, HTTPException, status, Depends
+from typing import Annotated
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+import models
+from database import Base, engine, get_db
+from schemas import VacationPostCreate, VacationPostResponse, VacationUpdate
 
-from fastapi import FastAPI, HTTPException, status
-from schemas import PostCreate, PostResponse
+Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
 
 
-posts:list[dict] = [
-  {
-    "id":1,
-    "name":"JavaScript",
-    "description":"JS is awesome"
 
-  },
-  {
-    "id":2,
-    "name":"Python",
-    "description":"build fast APIs with Python"
-  }
+vacationRequests:list[dict] = []
 
-]
+users:list[dict] = []
+
 
 @app.get('/')
 def home():
-  return "Hello from fastapi"
+  return "Welcome to the vacation request site"
 
-@app.get("/api/post", response_model=list[PostResponse])
-def get_posts():
-  return posts
+# VACATION MANAGMENT
 
-@app.post("/api/post", response_model=PostResponse, status_code=status.HTTP_201_CREATED)
-def create_post(post: PostCreate):
-  new_id = max(p["id"] for p in posts) + 1 if posts else 1
-  new_post = {
-    "id": new_id,
-    "author":post.author,
-    "type":post.type,
-    "description":post.description,
-    "status": "accepted",
-    "startDate": post.startDate,
-    "endDay": post.endDay
-  }
-  posts.append(new_post)
-  return new_post
+@app.get("/human-resources/vacation-managment", response_model=list[VacationPostResponse])
+def get_requests(db:Annotated[Session, Depends(get_db)]):
+  result = db.execute(select(models.Vacation))
+  extracting_vacation_requests = result.scalars().all()
+  return extracting_vacation_requests
 
-@app.get("/api/post/{post_id}", response_model=PostResponse)
-def get_posts(post_id:int):
-  for post in posts:
-    if post.get("id") == post_id:
-      return post
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+@app.get("/human-resources/vacation-managment/{request_id}", response_model=VacationPostResponse)
+def get_vacation_request(request_id:int, db:Annotated[Session, Depends(get_db)]):
+  result  = db.execute(select(models.Vacation).where(models.Vacation.id == request_id))
+  if not result:
+    raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="There is no conection with a database or something else went wrong")
+  requested_vacation = result.scalars().first()
+  if not requested_vacation:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="requested id does not exist")
+  return requested_vacation
+
+  
+  
+@app.patch("/human-resources/vacation-managment/{request_id}", response_model=VacationPostResponse, status_code=status.HTTP_206_PARTIAL_CONTENT)
+def update_request_status(request_id:int, updated_data:VacationUpdate, db:Annotated[Session, Depends(get_db)]):
+  result = db.execute(select(models.Vacation).where(models.Vacation.id == request_id))
+  if not result:
+    raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="There is no conection with a database or something else went wrong")
+  requested_vacation = result.scalars().first()
+  if requested_vacation:
+    requested_vacation.status = updated_data.status
+    if updated_data.status == "rejected":
+      if not updated_data.reason:
+        raise HTTPException(status_code=400, detail="reason field is required when status is rejected")
+      requested_vacation.reason = updated_data.reason
+    db.commit()
+    db.refresh(requested_vacation)
+    return requested_vacation
+  raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="requested id not found")
+ 
+    
+# EMPLOYEE VACATION REQUESTS
+
+@app.post("/myprofile/requestvacation", response_model=VacationPostResponse, status_code=status.HTTP_201_CREATED)
+def create_vacation_request(staff_id:int, post:VacationPostCreate, db:Annotated[Session, Depends(get_db)]):
+  # new_id = max(p["id"] for p in vacationRequests) + 1 if vacationRequests else 1
+  result = db.execute(select(models.Vacation).where(models.Vacation.staff_id == staff_id).where(models.Vacation.status == "pending"))
+  vacation_request = result.scalars().first()
+
+  if vacation_request:
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="A vacation request is still with status pending, wait until there is no request with status pending to make another request")
+  
+  new_request = models.Vacation(staff_id = staff_id,**post.model_dump())
+  db.add(new_request)
+  db.commit()
+  db.refresh(new_request)
+  return new_request
+
+
+    
+# make the get request for employees with request id
+@app.get("/myprofile/requestvacation/{request_id}", response_model=VacationPostResponse)
+def get_vacation_request_information(request_id:int, db:Annotated[Session, Depends(get_db)]):
+  result = db.execute(select(models.Vacation).where(models.Vacation.id == request_id))
+  vacation_requested = result.scalars().first()
+  if not vacation_requested:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="vacation request not found")
+  
+  return vacation_requested
+
+@app.delete("/myprofile/requestvacation/{request_id}", response_model=VacationPostResponse)
+def delete_vacation_request(request_vacation_id:int, db:Annotated[Session, Depends(get_db)]):
+  result = db.execute(select(models.Vacation).where(models.Vacation.id == request_vacation_id))
+  vacation_request = result.scalars().first()
+  if vacation_request:
+    db.delete(vacation_request)
+    db.commit()
+  raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Requested vacation id not found")
+
