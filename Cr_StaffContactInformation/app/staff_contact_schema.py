f77dc57a93b4a84ab8a70834144a421ad3ffb10b
MayraSRS04 <mayrarosas1@upb.edu>
2026-03-09 22:21:23 -0400

    Modified schema and service

--- Cr_StaffContactInformation/app/staff_contact_schema.py
+++ Cr_StaffContactInformation/app/staff_contact_schema.py
@@ -14,19 +14,28 @@ class StaffCreate(BaseModel):
     department_id: int
     specialty_id: int
 
+    profile_pic: Optional[str] = None
+    vacation_details: Optional[dict] = None
+
 
 class StaffResponse(BaseModel):
     id: int
     first_name: str
     last_name: str
     email: EmailStr
-    phone_number: str | None
+    phone_number: Optional[str]
     start_date: date
     status: str
     role_level: str
     department_id: int
     specialty_id: int
+
+    profile_pic: Optional[str]
+    vacation_details: Optional[dict]
+
     created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
 
 class StaffUpdate(BaseModel):
     first_name: Optional[str] = Field(None, min_length=1, max_length=100)
@@ -36,4 +45,9 @@ class StaffUpdate(BaseModel):
     status: Optional[str] = None
     role_level: Optional[str] = None
 
-    model_config = ConfigDict(from_attributes=True, extra="forbid")
+    profile_pic: Optional[str] = None
+
+    model_config = ConfigDict(
+        from_attributes=True,
+        extra="forbid"
+    )
